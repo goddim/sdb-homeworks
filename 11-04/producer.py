0ac06f8600c22c041d0571c8e6336aeba428e4b3
goddim <132663924+goddim@users.noreply.github.com>
2023-09-03 19:10:57 +0800

    producer.py

--- 11-04/producer.py
+++ 11-04/producer.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.250'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
